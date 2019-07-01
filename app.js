ac274619027c9cc5eb75ac45095b957f4be7354f
Mike <mike.huang.mikank@gmail.com>
2019-07-01 16:22:11 +0800

    Add mongoose and create record and user schema

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 // Include modules
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 // Set up server related variable
 const port = 3000
+
+// connect to MongoDB, it will return a Connection object
+mongoose.connect('mongodb://127.0.0.1/expense-tracker', {
+  useNewUrlParser: true, useCreateIndex: true
+})
+
+const db = mongoose.connection
+
+// connection error
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// connection seccess
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// Include Models
+const User = require('./models/user')
+const Record = require('./models/record')
 
 app.get('/', (req, res) => {
   res.send('landing page')
---
+++ models/record.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  record: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
