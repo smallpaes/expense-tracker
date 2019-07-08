81f91a51858614b81ddb75e5cfa584d921e98514
Mike <mike.huang.mikank@gmail.com>
2019-07-08 15:39:36 +0800

    Refactor getChartData function

--- date-process.js
+++ date-process.js
@@ -13,14 +13,8 @@ module.exports = {
     return `${date.split('/')[1]}/${date.split('/')[0]}`
   },
   getChartData: records => {
-    const chartData = [0, 0, 0, 0, 0]
-    records.forEach(record => {
-      record.category === '家居物業' ? chartData[0] += record.amount
-        : record.category === '交通出行' ? chartData[1] += record.amount
-          : record.category === '休閒娛樂' ? chartData[2] += record.amount
-            : record.category === '餐飲食品' ? chartData[3] += record.amount
-              : chartData[4] += record.amount
-    })
-    return chartData
+    const categories = { '家居物業': 0, '交通出行': 0, '休閒娛樂': 0, '餐飲食品': 0, '其他': 0 }
+    records.forEach(record => { categories[record.category] += record.amount })
+    return Object.values(categories)
   }
 }
