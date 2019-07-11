24e1c0c6d8957b205cf7e0ff36ca7f39eadef181
Mike <mike.huang.mikank@gmail.com>
2019-07-11 17:45:25 +0800

    Refactor expense controller with async/await

--- controllers/expense.js
+++ controllers/expense.js
@@ -4,13 +4,11 @@ const { validationResult } = require('express-validator')
 
 module.exports = {
   getNewExpense: (req, res) => {
-    console.log(req.user)
     res.render('form', { formCSS: true, formValidateJS: true })
   },
-  postNewExpense: (req, res) => {
+  postNewExpense: async (req, res) => {
     // retrieve input data
     const { name, date, category, amount } = req.body
-    console.log(date)
     // Find all validation errors in the req in a object
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
@@ -29,21 +27,25 @@ module.exports = {
       amount: amount,
       userId: req.user._id
     })
-    // save the document to record collection
-    newRecord.save((err, expense) => {
-      if (err) return console.log(err)
+
+    try {
+      // save the document to record collection
+      await newRecord.save()
       res.redirect('/')
-    })
+    } catch (err) {
+      console.log(err)
+    }
   },
-  getEditExpense: (req, res) => {
-    // find the document based on id 
-    Record.findOne({ _id: req.params.id, userId: req.user._id })
-      .then(record => {
-        res.render('form', { formCSS: true, record, formValidateJS: true, isEditMode: true })
-      })
-      .catch(err => console.log(err))
+  getEditExpense: async (req, res) => {
+    try {
+      // find the document based on id 
+      const record = await Record.findOne({ _id: req.params.id, userId: req.user._id })
+      res.render('form', { formCSS: true, record, formValidateJS: true, isEditMode: true })
+    } catch (err) {
+      console.log(err)
+    }
   },
-  postEditExpense: (req, res) => {
+  postEditExpense: async (req, res) => {
     // get all data from data input
     const { name, date, category, amount } = req.body
     // Find all validation errors in the req in a object
@@ -57,31 +59,34 @@ module.exports = {
         errorMessages: errors.array()
       })
     }
-    // find the document based on id
-    Record.findOne({ _id: req.params.id, userId: req.user._id })
-      .then(record => {
-        // update document info based on form input
-        record.name = name
-        record.date = date
-        record.category = category
-        record.amount = amount
 
-        // save the document to database
-        record.save()
-          .then(record => res.redirect('/'))
-          .catch(err => console.log(err))
-      })
-      .catch(err => console.log(err))
+    try {
+      // find the document based on id
+      const record = await Record.findOne({ _id: req.params.id, userId: req.user._id })
+
+      // update document info based on form input
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+
+      // save the document to database
+      await record.save()
+      res.redirect('/')
+
+    } catch (err) {
+      console.log(err)
+    }
   },
-  deleteExpense: (req, res) => {
-    // find the document based on id
-    Record.findOne({ _id: req.params.id, userId: req.user._id })
-      .then(record => {
-        record.remove((err, record) => {
-          if (err) return console.log(err)
-          res.redirect('/')
-        })
-      })
-      .catch(err => console.log(err))
+  deleteExpense: async (req, res) => {
+    try {
+      // find the document based on id
+      const record = await Record.findOne({ _id: req.params.id, userId: req.user._id })
+      // remove the record
+      await record.remove()
+      res.redirect('/')
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
