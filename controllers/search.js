77a27f8e688910d992991ff0ba60696e7e0d74e9
Mike <mike.huang.mikank@gmail.com>
2019-07-11 17:31:19 +0800

    Refactor search controller with async/await

--- controllers/search.js
+++ controllers/search.js
@@ -3,7 +3,7 @@ const Record = require('../models/record')
 const { getDateCriteria, getFormatedMonth, getChartData } = require('../date-process')
 
 module.exports = {
-  getSearchExpense: (req, res) => {
+  getSearchExpense: async (req, res) => {
     // keep filtered options
     const { month, category, defaultCategory, defaultMonth, monthReset, categoryReset } = req.query
     const selectedMonth = (monthReset || (!month && !defaultMonth)) ? null
@@ -16,32 +16,36 @@ module.exports = {
     const dateCriteria = getDateCriteria(selectedMonth)
     const months = []
 
-    // find all record
-    Record.find({ userId: req.user._id }, null, { sort: { date: 'desc' } })
-      .then(records => {
-        // find month options for filter
-        records.forEach(record => {
-          const displayDate = getFormatedMonth(record, false)
-          if (months.includes(displayDate)) { return }
-          months.push(displayDate)
-        })
-        return Record.find({
-          $and: [
-            dateCriteria,
-            categoryCriteria,
-            { userId: req.user._id }
-          ]
-        })
-          .sort({ date: 'desc' })
+    try {
+      // find all records
+      const allRecords = await Record.find({ userId: req.user._id }, null, { sort: { date: 'desc' } })
+
+      // get month options for filter
+      allRecords.forEach(record => {
+        const displayDate = getFormatedMonth(record, false)
+        if (months.includes(displayDate)) { return }
+        months.push(displayDate)
       })
-      .then(records => {
-        // check if any record is found
-        const isEmptyRecord = records.length ? false : true
-        // find total expense
-        const totalAmount = records.reduce((acc, cur) => acc + cur.amount, 0)
-        // get chart data
-        const chartData = getChartData(records)
-        res.render('index', { indexCSS: true, records, totalAmount, selectedCategory, selectedMonth, months, chartData, showChart: true, isEmptyRecord })
+
+      // find matching records
+      const displayRecords = await Record.find({
+        $and: [
+          dateCriteria,
+          categoryCriteria,
+          { userId: req.user._id }
+        ]
       })
+        .sort({ date: 'desc' })
+
+      // check if any record is found
+      const isEmptyRecord = displayRecords.length ? false : true
+      // find total expense
+      const totalAmount = displayRecords.reduce((acc, cur) => acc + cur.amount, 0)
+      // get chart data
+      const chartData = getChartData(displayRecords)
+      res.render('index', { indexCSS: true, records: displayRecords, totalAmount, selectedCategory, selectedMonth, months, chartData, showChart: true, isEmptyRecord })
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
