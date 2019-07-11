7c9f5f7691bdbcfaf5c677e3a7a34e08ea35454a
Mike <mike.huang.mikank@gmail.com>
2019-07-11 17:16:16 +0800

    Refactor home with async/await

--- controllers/home.js
+++ controllers/home.js
@@ -3,26 +3,29 @@ const Record = require('../models/record')
 const { getFormatedMonth, getChartData } = require('../date-process')
 
 module.exports = {
-  getHome: (req, res) => {
-    // retrieve all expense from record collection
-    Record.find({ userId: req.user._id })
-      .sort({ date: 'desc' })
-      .exec((err, records) => {
-        if (err) return console.log(err)
-        // find total expense
-        const totalAmount = records.reduce((acc, cur) => acc + cur.amount, 0)
-        // get chart data
-        const chartData = getChartData(records)
-        // check if any record is found
-        const isEmptyRecord = records.length ? false : true
-        // find total month
-        const months = []
-        records.forEach(record => {
-          const displayDate = getFormatedMonth(record)
-          if (months.includes(displayDate)) { return }
-          months.push(displayDate)
-        })
-        res.render('index', { indexCSS: true, records, totalAmount, months, chartData, showChart: true, isEmptyRecord })
+  getHome: async (req, res) => {
+    try {
+      // retrieve all expense from record collection
+      const records = await Record.find({ userId: req.user._id })
+        .sort({ data: 'desc' })
+        .exec()
+
+      // find total expense
+      const totalAmount = records.reduce((acc, cur) => acc + cur.amount, 0)
+      // get chart data
+      const chartData = getChartData(records)
+      // check if any record is found
+      const isEmptyRecord = records.length ? false : true
+      // find total month
+      const months = []
+      records.forEach(record => {
+        const displayDate = getFormatedMonth(record)
+        if (months.includes(displayDate)) { return }
+        months.push(displayDate)
       })
+      res.render('index', { indexCSS: true, records, totalAmount, months, chartData, showChart: true, isEmptyRecord })
+    } catch (err) {
+      return console.log(err)
+    }
   }
 }
